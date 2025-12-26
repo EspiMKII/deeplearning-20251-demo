c1c7614d475d6c2689cc54b6503b06a14d567475
EspiMKII <therealrobin14@gmail.com>
2025-12-26 14:34:23 +0700

    Figuring out how jinja-html works...

--- app/src/base.py
+++ app/src/base.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 import os
 
 srcpath = os.path.dirname(__file__)
@@ -6,9 +6,11 @@ apppath = os.path.dirname(srcpath)
 staticpath = os.path.join(apppath, 'static')
 templatepath = os.path.join(apppath, 'templates')
 app = Flask(__name__, static_folder=staticpath, template_folder=templatepath)
+print(app.static_folder)
+print(app.template_folder)
 
-@app.route("/test")
-def test():
-    return "man fuck yall"
+@app.route("/base")
+def base():
+    return render_template("base.html")
 
 if __name__ == "__main__": app.run(debug=True)
