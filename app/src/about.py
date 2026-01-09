7de569c4a4df7a37969cd4b7295cee461e4cc0e3
EspiMKII <therealrobin14@gmail.com>
2026-01-09 15:39:36 +0700

    About page

--- app/src/about.py
+++ app/src/about.py
@@ -1,8 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
+from form_id_handler import formIdHandler
 
 about_bp = Blueprint('about', __name__, url_prefix='/about')
 
 # domain/about
 @about_bp.route('/', methods=('GET', 'POST'))
 def about():
-    return 'about page'
+    if request.method == "POST":
+        form_id = request.form.get("form_id")
+        return formIdHandler(form_id)
+    return render_template("about.html")
